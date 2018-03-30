7a41433f16f31d0be28b30a51e7704a162a9429d
Timothy J. Warren <tim@timshomepage.net>
2018-03-30 18:33:20 -0400

    Move electron scripts to their own folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { version, Component } from 'inferno';
-import { Container, Row } from './components/Grid'
+import { version } from 'inferno';
+import { BrowserRouter, Route } from 'inferno-router'
+import { Container, Row } from './components/Grid';
 
-class App extends Component {
-	render () {
-		return (
-			<Container className="App">
-				<Row>
-					<header className="App-header">
-						<h1>{`Welcome to Inferno ${version}`}</h1>
-					</header>
-				</Row>
-				<Row>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to reload.
-					</p>
-				</Row>
-			</Container>
-		);
-	}
-}
+const App  = () => (
+	<BrowserRouter>
+		<Container className="App">
+			<Row>
+				<header className="App-header">
+					<h1>{`Welcome to Inferno ${version}`}</h1>
+				</header>
+			</Row>
+			<Row>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</Row>
+		</Container>
+	</BrowserRouter>
+);
 
 export default App;
