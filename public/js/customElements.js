c0ee4f06d4c143c3f4f019195fbb24c4c46886b1
Timothy Warren <twarren@nabancard.com>
2018-04-04 11:21:38 -0400

    A little housecleaning

--- public/js/customElements.js
+++
@@ -1,5 +0,0 @@
-/* class BSContainer extends HTMLElement {}
-class FilmExifElement extends HTMLElement {}
-
-customElements.define('bs-container', BSContainer);
-customElements.define('film-exif', FilmExifElement); */
--- src/components/FormBlock.js
+++ src/components/FormBlock.js
@@ -8,18 +8,17 @@ import {
 export const FormBlock = ({
 	children,
 	label,
-	name, 
-	type='text', 
+	type = 'text',
 	...props
 }) => {
 	const formElement = (children !== undefined)
 		? children
-		: <Input id={name} name={name} type={type} {...props} />
+		: <Input id={props.name} name={props.name} type={type} {...props} />;
 
 	return (
 		<Col xs={12} sm={6} md={3}>
 			<FormGroup>
-				<Label for={name}>{label}</Label>
+				<Label for={props.name}>{label}</Label>
 				{formElement}
 			</FormGroup>
 		</Col>
