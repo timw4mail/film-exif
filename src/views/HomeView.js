c2426baa1af254718ff4e86d5f508c48fad661fd
Timothy Warren <twarren@nabancard.com>
2018-05-01 09:53:09 -0400

    Ugly progress commit

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,4 +1,5 @@
 import {
+	Alert,
 	Button,
 	Col,
 	Container,
@@ -41,30 +42,35 @@ function showErrorDialog () {
 
 export const HomeView = (props) => {
 	return (
-		<Jumbotron onDrop={handleDrop} onDragover={handleDragOver}>
-			<Container className="App">
-				<Row>
-					<header className="App-header">
-						<h1>Welcome to Film Exif</h1>
-					</header>
-				</Row>
-				<Row>
-					<p className="App-intro">
-						Drop files here.
-					</p>
-				</Row>
-				<Row>
-					<Col md={4}>
-						<Button onClick={showOpenDialog}>Show Open Dialog</Button>
-					</Col>
-					<Col md={4}>
-						<Button onClick={showSaveDialog}>Show Save Dialog</Button>
-					</Col>
-					<Col md={4}>
-						<Button onClick={showErrorDialog}>Show Error Dialog</Button>
-					</Col>
-				</Row>
-			</Container>
-		</Jumbotron>
+		<main>
+			<Alert color="info">
+			This is a work in progress
+			</Alert>
+			<Jumbotron onDrop={handleDrop} onDragover={handleDragOver}>
+				<Container className="App">
+					<Row>
+						<header className="App-header">
+							<h1>Welcome to Film Exif</h1>
+						</header>
+					</Row>
+					<Row>
+						<p className="App-intro">
+							Drop files here.
+						</p>
+					</Row>
+					<Row>
+						<Col md={4}>
+							<Button onClick={showOpenDialog}>Show Open Dialog</Button>
+						</Col>
+						<Col md={4}>
+							<Button onClick={showSaveDialog}>Show Save Dialog</Button>
+						</Col>
+						<Col md={4}>
+							<Button onClick={showErrorDialog}>Show Error Dialog</Button>
+						</Col>
+					</Row>
+				</Container>
+			</Jumbotron>
+		</main>
 	);
 };
