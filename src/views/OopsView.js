74479b20500e0ebf3102d0e0fe5733be5fa5fea3
Timothy Warren <twarren@nabancard.com>
2018-05-09 09:19:48 -0400

    Update dependencies

--- src/views/OopsView.js
+++ src/views/OopsView.js
@@ -1,6 +1,6 @@
 import { Alert, Row } from '//components/Bootstrap';
 
-export const OopsView = (props) => (
+export const OopsView = () => (
 	<Row className="align-items-center justify-content-center full-height">
 		<Alert className="abs-center" color="danger" style={{padding: '5em'}} tag="main">
 			<h1>Oops!</h1>
